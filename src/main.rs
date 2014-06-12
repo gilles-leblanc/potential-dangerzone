7c43631cd03cb9864751e78ca063182edefe3473
Gilles Leblanc <gilles.leblanc@gmail.com>
2014-06-11 22:20:47 -0400

    Create drop points for each pass

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,21 @@
 mod map_generator;
 
 fn main() {
-  let mut height_map: Vec<int> = Vec::from_fn(1600, |_| 0);
-
+  let size = 40;
   let number_of_drop_points = 5;
   let min_particles = 100;
   let max_particles = 400;
   let number_of_passes = 4;
   let particle_stability_radius = 1;
 
-  let drops = map_generator::create_drop_points(number_of_drop_points,
-                                                min_particles,
-                                                max_particles);
+  let mut height_map: Vec<int> = Vec::from_fn(size * size, |_| 0);
+
+  let drops: Vec<Box<Vec<int>>>;
+  drops = Vec::from_fn(number_of_passes,
+                       |_| map_generator::create_drop_points(number_of_drop_points,
+                                                             min_particles,
+                                                             max_particles));
+
 
   for i in drops.iter() {
     println!("{}", i);
