bc1e1872c1fa0771538504dc25e94b60363f2a99
Gilles Leblanc <gilles.leblanc@gmail.com>
2014-06-12 21:42:54 -0400

    Creates struct for Height Map

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-// File with main method. Can be used to start the generator without
-// including it as a library from an external program.
+use map_generator::HeightMap;
 mod map_generator;
 
 fn main() {
@@ -10,7 +9,7 @@ fn main() {
   let number_of_passes = 4;
   let particle_stability_radius = 1;
 
-  let mut height_map: Vec<int> = Vec::from_fn(size * size, |_| 0);
+  let mut height_map: HeightMap = HeightMap::new(size);
 
   let drops: Vec<Box<Vec<int>>>;
   drops = Vec::from_fn(number_of_passes,
@@ -18,8 +17,7 @@ fn main() {
                                                              min_particles,
                                                              max_particles));
 
-
-  for i in drops.iter() {
-    println!("{}", i);
+  for drop_points in drops.iter() {
+    map_generator::drop_particles(drop_points.as_slice(), &height_map);
   }
 }
--- src/map_generator.rs
+++ src/map_generator.rs
@@ -1,6 +1,17 @@
 use std::rand::Rng;
 use std::rand;
 
+pub struct HeightMap {
+  map: Vec<int>,
+  size: uint,
+}
+
+impl HeightMap {
+  pub fn new(size: uint) -> HeightMap {
+    HeightMap { map: Vec::from_fn(size * size, |_| 0), size: size }
+  }
+}
+
 pub fn create_drop_points(number_of_drop_points: uint, min_particles: int, max_particles: int) -> Box<Vec<int>> {
   let mut drops: Vec<int> = Vec::with_capacity(number_of_drop_points);
   let mut rng = rand::task_rng();
@@ -10,4 +21,16 @@ pub fn create_drop_points(number_of_drop_points: uint, min_particles: int, max_p
   }
 
   box drops
+}
+
+pub fn drop_particles(drops: &[int], height_map: &HeightMap) {
+  for drop in drops.iter() {
+    let drop_coord_x = height_map.size / 2;
+    let drop_coord_y = height_map.size / 2;
+
+    for _ in range(1, *drop) {
+      // drop a particle
+      
+    }
+  }
 }
